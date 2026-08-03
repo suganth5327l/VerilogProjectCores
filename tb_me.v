ecbfc7a77ed5bd22b2e5000fc5a135eaae12ae22
Suganth Saravanan <77095202+suganth5327l@users.noreply.github.com>
2026-08-03 07:19:30 +0530

    rev2 for the refined mem block
    rev2 of the testbench for the refined mem block and added clock for better optimization

--- tb_me.v
+++ tb_me.v
@@ -1,12 +1,18 @@
-//ver 1 rev2/6/26
+//ver 2 rev3/6/26
 
 `timescale 1ns/1ps
 module tb_me;
-reg cs,we;
+reg cs,we,clk;
 reg [2:0] i,adr;
 wire [2:0] a;
-me i1(cs,we,i,adr,a);
+me i1(clk,cs,we,i,adr,a);
 initial begin
+clk=1'b0;
+forever #5 clk=~clk;
+end
+initial begin
+    $dumpfile("me.vcd");
+    $dumpvars(0,tb_me);
 cs=1'b1;we=1'b0;
 #10 adr = 3'b010; i = 3'b101;
 #10 we=1'b1; adr=3'b000;
